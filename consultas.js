1016d1937b8baa972ba1958380541f42097fd003
Ayleen <ayleen.martinez16@gmail.com>
2025-01-30 17:04:51 -0300

    put y delete mas try catch

--- consultas.js
+++ consultas.js
@@ -1,30 +1,63 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: '4324',
-    database: 'likeme',
-    allowExitOnIdle: true
+  host: "localhost",
+  user: "postgres",
+  password: "4324",
+  database: "likeme",
+  allowExitOnIdle: true,
 });
 
 const getDate = async () => {
+  try {
     const result = await pool.query("SELECT NOW()");
     console.log(result);
-}
+  } catch (error) {
+    console.error("error al obtener fecha:", error);
+  }
+};
 
 const agregarPost = async (titulo, img, descripcion, likes) => {
+  try {
     const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3, $4)";
     const values = [titulo, img, descripcion, likes];
     const result = await pool.query(consulta, values);
     console.log("Post agregado");
-    };
+  } catch (error) {
+    console.error("Error al agregar el post:", error);
+  }
+};
 
 const obtenerPost = async () => {
-    const { rows } = await pool.query("SELECT * FROM posts")
-    console.log("Funciono GET")
-    return rows
-    };
-    
+  try {
+    const { rows } = await pool.query("SELECT * FROM posts");
+    console.log("Posts obtenidos");
+    return rows;
+  } catch (error) {
+    console.error("Error al obtener el post:", error);
+  }
+};
 
-module.exports = {agregarPost, obtenerPost };
-    
+const actualizarPost = async (titulo, img, descripcion, likes, id) => {
+  try {
+    const consulta =
+      "UPDATE posts SET titulo = $1, img = $2, descripcion = $3, likes = $4 WHERE id = $5";
+    const values = [titulo, img, descripcion, likes, id];
+    const result = await pool.query(consulta, values);
+    console.log("Post actualizado");
+  } catch (error) {
+    console.error("Error al actualizar el post:", error);
+  }
+};
+
+const eliminarPost = async (id) => {
+  try {
+    const consulta = "DELETE FROM posts WHERE id = $1";
+    const values = [id];
+    const result = await pool.query(consulta, values);
+    console.log("Post eliminado");
+  } catch (error) {
+    console.error("Error al eliminar el post:", error);
+  }
+};
+
+module.exports = { agregarPost, obtenerPost, actualizarPost, eliminarPost };
--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-const { agregarPost, obtenerPost } = require('./consultas');
+const {
+  agregarPost,
+  obtenerPost,
+  actualizarPost,
+  eliminarPost,
+} = require("./consultas");
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors());
+app.use(express.json());
 
 app.listen(3000, console.log("¡Servidor encendido!"));
 
-app.use(express.json());
-
 app.get("/posts", async (req, res) => {
-    const posts = await obtenerPost()
-    res.json(posts)
+  try {
+    const posts = await obtenerPost();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).send("Error al obtener los posts");
+  }
 });
 
 app.post("/posts", async (req, res) => {
-    const { titulo, img, descripcion, likes } = req.body
-    await agregarPost( titulo, img, descripcion, likes )
-    res.send("Post agregado con éxito")
+  try {
+    const { titulo, img, descripcion, likes } = req.body;
+    await agregarPost(titulo, img, descripcion, likes);
+    res.send("Post agregado con éxito");
+  } catch (error) {
+    res.status(500).send("Error al agregar un post");
+  }
 });
 
+app.put("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { titulo, img, descripcion, likes } = req.body;
+    await actualizarPost(titulo, img, descripcion, likes, id);
+    res.send("Post modificado con éxito");
+  } catch (error) {
+    res.status(500).send("Error al actualizar los posts");
+  }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await eliminarPost(id);
+    res.send("Post eliminado con éxito");
+  } catch (error) {
+    res.status(500).send("Error al eliminar el post");
+  }
+});
